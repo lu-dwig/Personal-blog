09348bbce0aee1fa9f763b68bf03e25751cd0914
ludwig <murimiludwig@gmail.com>
2022-05-14 22:44:28 +0300

    this will add the name form ofrm for user

--- blog.py
+++ blog.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "high tym to create your key"
 
+# Create a form user function
+class NameForm(FlaskForm):
+    name = StringField("What is your name", validators=[DataRequired()])
+    submit = SubmitField('Submit')
 # Create routes
 @app.route('/')
 
@@ -29,4 +36,13 @@ def page_not_found(error):
 @app.errorhandler(500)
 def page_not_found(error):
     return render_template("404.html", error=error), 404 
-    
+
+# Create Name Page 
+@app.route('/name',methods=['GET','POST'])
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html",name=name,form=form)

269d1b4f580bab8025ca9646e3ba28e2d3801c4e
ludwig <murimiludwig@gmail.com>
2022-05-20 14:47:58 +0300

    creation of the of the picture saving route

--- personal_blog/routes.py
+++ personal_blog/routes.py
@@ -57,3 +57,23 @@ def login():
             flash('Login Unsuccessful. Please check username and password','danger')
     return render_template('login.html', title='Login', form=form)
 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/photos', picture_fn)
+    
+    
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
+    
